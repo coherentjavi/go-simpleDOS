7cd8e8bc363bf639a4cf44f06517cd7ba771ba68
javiMax <coherentsheafjavi@proton.me>
2025-09-30 21:12:20 -0600

    removed promopt asking for port twice in main

--- src/main.go
+++ src/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
 func main() {
@@ -9,14 +10,12 @@ func main() {
 	var packets, port int
 	fmt.Print("Please enter the IP being spoofed, destination IP and number of packets that'll be sent in the flood (all separated by a space):")
 	fmt.Scan(&src, &destn, &packets)
-	fmt.Println("We'll now scan the ports on the device with the destination IP")
-	fmt.Scan(&port)
-	
+
 	start := time.Now()
 	ScanPorts(destn)
 	elapsed := time.Since(start)
 	fmt.Printf("***The scan took %v***\n", elapsed)
-	
+
 	fmt.Println("Which port do you want SYN flood?")
 	fmt.Scan(&port)
 
